d72f680a8b86daca0ada83b3e22669f00f3cc45b
jubileelin <jubilee.l.0921@gmail.com>
2025-04-12 23:27:29 -0700

    fixed bug

--- exhibit/src/pages/contentPage/templates/template_BC.jsx
+++ exhibit/src/pages/contentPage/templates/template_BC.jsx
@@ -1,17 +1,18 @@
-import SimpleAnimatedText from "../SimpleAnimatedText"
+import AnimatedText from "../animated-text.jsx"
+import "../../../styles/text-animation.css"
 
 const TemplateBC = ({ title, topContent, bottomContent, imageUrl }) => {
   return (
     <div className="w-[48%] flex flex-col p-6 rounded-lg">
       <h3 className="text-3xl font-lovers font-bold text-black mb-4">
-        <SimpleAnimatedText text={title} />
+        <AnimatedText text={title} />
       </h3>
       <div className="flex-1">
         <p className="text-base font-imfell text-black font-medium leading-relaxed mb-4">
-          <SimpleAnimatedText text={topContent} />
+          <AnimatedText text={topContent} />
         </p>
         <p className="text-base font-imfell text-black font-medium leading-relaxed mb-4">
-          <SimpleAnimatedText text={bottomContent} />
+          <AnimatedText text={bottomContent} />
         </p>
       </div>
       <div className="w-full h-48 bg-gray-200/50 backdrop-blur-sm rounded-lg mx-auto">
